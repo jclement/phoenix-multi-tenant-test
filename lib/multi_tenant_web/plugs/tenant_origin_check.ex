e883b0b0f024cefe9cd03ae57e5f1e29226ffbdd
Jeff Clement <jeffrey.clement@gmail.com>
2024-02-18 13:17:58 -0700

    logging

--- lib/multi_tenant_web/plugs/tenant_origin_check.ex
+++ lib/multi_tenant_web/plugs/tenant_origin_check.ex
@@ -5,9 +5,10 @@ defmodule MultiTenantWeb.Plugs.TenantOriginCheck do
   if Application.compile_env(:multi_tenant, :validate_origin) do
     @doc false
     def call(conn, _opts) do
-      origin = Plug.Conn.get_req_header(conn, "origin")
+      origin = Plug.Conn.get_req_header(conn, "origin") |> IO.inspect(label: "Origin")
 
-      if origin in MultiTenant.Tenants.list_origins() do
+      if (origin in MultiTenant.Tenants.list_origins())
+         |> IO.inspect(label: "Configured Origins") do
         conn
       else
         conn |> Plug.Conn.send_resp(403, "Forbidden") |> Plug.Conn.halt()
